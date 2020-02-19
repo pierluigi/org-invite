e2381ad923ae998ed13154698f6fe9b9893e6f8c
Pierluigi Cau <pierluigi@github.com>
2020-02-19 20:21:30 +0100

    Refactor, cleanup

--- cli.js
+++ cli.js
@@ -1,64 +1,65 @@
 #!/usr/bin/env node
 
 const program = require("commander");
-const prettyBytes = require("pretty-bytes");
 const chalk = require("chalk");
 const _ = require("lodash");
-const moment = require("moment");
 var inquirer = require("inquirer");
 const Octokit = require("@octokit/rest");
-
 const dotenv = require("dotenv");
+
+const state = {
+  teamExists: false,
+  teamId: 0,
+  teamUrl: null,
+  teamSlug: null
+};
+
+var octokit;
 
 dotenv.config();
 
 program.option(
   "-t, --token <PAT>",
-  "Your GitHub PAT (leave blank for prompt or set $GH_PAT)"
+  "Your GitHub PAT (leave blank for prompt or set $GH_PAT)",
+  process.env.GH_PAT
 );
 program.option(
   "-u, --user <username>",
-  "Your GitHub username (leave blank for prompt or set $GH_USER)"
+  "Your GitHub username (leave blank for prompt or set $GH_USER)",
+  process.env.GH_USER
 );
 program.option(
   "-o, --org <organization>",
-  "Organization name (leave blank for prompt or set $GH_ORG)"
+  "Organization name (leave blank for prompt or set $GH_ORG)",
+  process.env.GH_ORG
 );
 
-program.option("-t, --team <team>", "Team slug");
+program.option("-s, --slug <team-slug>");
 
 program.parse(process.argv);
 
-let teamExists = false;
-let teamId = 0;
-let teamUrl = null;
-let teamSlug = null;
-
-var octokit;
+const die = msg => {
+  const ui = new inquirer.ui.BottomBar();
+  ui.log.write(`${chalk.red("[ERROR]")} ${msg}`);
+  process.exit(1);
+};
 
 const findTeam = async ({ owner, org, team }) => {
-  var ui = new inquirer.ui.BottomBar();
-
-  // UI
-  // var i = 4;
-  // var loader = ["/ Loading", "| Loading", "\\ Loading", "- Loading"];
-  // var ui = new inquirer.ui.BottomBar({ bottomBar: loader[0] });
-
-  // const loadingInterval = setInterval(() => {
-  //   ui.updateBottomBar(loader[i++ % 4]);
-  // }, 200);
+  const ui = new inquirer.ui.BottomBar();
 
   ui.log.write(
-    `${chalk.dim("[1/3]")} Verifying team ${chalk.green(team)} exists...`
+    `${chalk.dim("[1/3]")} Verifying team ${chalk.green(
+      `${org}/${team}`
+    )} exists...`
   );
 
   try {
-    // clearInterval(loadingInterval);
     var { data } = await octokit.teams.getByName({ org, team_slug: team });
   } catch (e) {
-    // clearInterval(loadingInterval);
     if (e.status === 404) {
-      teamExists = false;
+      state.teamExists = false;
+    } else {
+      die(e.message);
     }
   }
 
@@ -66,22 +67,20 @@ const findTeam = async ({ owner, org, team }) => {
     _.get(data, "organization", false) &&
     _.get(data, "organization.login", false) === org
   ) {
-    teamExists = true;
-    teamId = data.id;
-    teamUrl = data.html_url;
-    teamSlug = data.slug;
+    state.teamExists = true;
+    state.teamUrl = data.html_url;
+    state.teamSlug = data.slug;
   }
 
-  if (teamExists) {
+  if (state.teamExists) {
     ui.log.write(`${chalk.dim("[2/3]")} Team ${chalk.green(team)} found.`);
   }
 };
 
 async function createTeam({ owner, org, team }) {
-  if (teamExists) return;
+  if (state.teamExists) return;
 
-  var ui = new inquirer.ui.BottomBar();
-
+  const ui = new inquirer.ui.BottomBar();
   ui.log.write(`${chalk.dim("[2/3]")} Team ${chalk.green(team)} not found. `);
 
   await inquirer
@@ -89,19 +88,16 @@ async function createTeam({ owner, org, team }) {
       {
         type: "confirm",
         name: "createTeam",
-        message: `Create it now?`
+        message: `Create a new team now?`
       },
       {
         type: "input",
         name: "newTeamName",
-        default: function() {
-          return program.team;
-        },
-        message: "Team name",
-        when: function({ createTeam }) {
-          return createTeam;
-        }
+        message: "Name of the team",
+        default: () => team,
+        when: ({ createTeam }) => createTeam
       },
+
       {
         type: "input",
         name: "teamDescription",
@@ -115,25 +111,29 @@ async function createTeam({ owner, org, team }) {
       if (!createTeam) {
         process.exit();
       } else {
-        var {
-          data: { id, html_url, slug }
-        } = await octokit.teams.create({
-          org,
-          name: newTeamName,
-          privacy: "secret",
-          description: teamDescription
-        });
+        try {
+          var {
+            data: { id, html_url, slug }
+          } = await octokit.teams.create({
+            org,
+            name: newTeamName,
+            privacy: "secret",
+            description: teamDescription
+          });
 
-        teamExists = true;
-        teamId = id;
-        teamUrl = html_url;
-        teamSlug = slug;
+          state.teamExists = true;
+          state.teamUrl = html_url;
+          state.teamSlug = slug;
+          state.teamId = id;
+        } catch (e) {
+          die(e.message);
+        }
       }
     });
 }
 
 async function inviteMembers({ org, team }) {
-  var ui = new inquirer.ui.BottomBar();
+  const ui = new inquirer.ui.BottomBar();
 
   await inquirer
     .prompt([
@@ -161,14 +161,14 @@ async function inviteMembers({ org, team }) {
           // assume valid email
           const res = await octokit.orgs.createInvitation({
             org,
-            team_ids: [teamId],
+            team_ids: [state.teamId],
             email: i
           });
         } else {
           // assume valid username
           const res = await octokit.teams.addOrUpdateMembershipInOrg({
             org,
-            team_slug: teamSlug,
+            team_slug: state.teamSlug,
             username: i
           });
         }
@@ -178,7 +178,7 @@ async function inviteMembers({ org, team }) {
       }, []);
 
       ui.log.write(`${chalk.dim("[OK]")} Done. Review invitations at:`);
-      ui.log.write(teamUrl);
+      ui.log.write(state.teamUrl);
     });
 }
 
@@ -188,25 +188,19 @@ inquirer
       type: "password",
       name: "PAT",
       message: "What's your GitHub PAT?",
-      default: function() {
-        return program.token || process.env.GH_PAT;
-      }
+      default: () => program.token
     },
     {
       type: "input",
       name: "owner",
       message: "Your username?",
-      default: function() {
-        return program.user || process.env.GH_USER;
-      }
+      default: () => program.user
     },
     {
       type: "input",
       name: "org",
       message: "Which organization?",
-      default: function() {
-        return program.org || process.env.GH_ORG;
-      }
+      default: () => program.org
     },
     {
       type: "input",
